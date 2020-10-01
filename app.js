0d4917151f5ce1ccf7a1af720e0f98ba37477637
Jake Leahy <jake@leahy.dev>
2020-10-01 10:20:43 +1000

    Cleanup the if statements
    Clean up the if statements so it is not repeating checks everytime

--- app.js
+++ app.js
@@ -16,27 +16,28 @@ client.on('ready', () => {
 });
 
 client.on('message', async msg => {
-	if (msg.isMentioned(client.user) && msg.author.id !== '220352311422091264') {
-		console.log(msg.author);
-		msg.reply('Hey! I\'m not available at the moment. I\'ll get back to you as soon as possible.');
-	} else if (msg.author.id === '220352311422091264' && msg.channel.id === '694399153228218468') {
+	if (msg.author.id !== '220352311422091264') {
+		if (msg.isMentioned(client.user)) {
+			console.log(msg.author);
+			msg.reply('Hey! I\'m not available at the moment. I\'ll get back to you as soon as possible.');
+		} else if (msg.content.startsWith("sarthak say ")) {
+			msg.channel.send(msg.content.slice(12, msg.content.length));
+		} else if (msg.content.startsWith("sarthak upload")) {
+			await message.channel.send("Send your file please..");
+			let filter = m => m.author == message.author; //use that message only if the author is the same
+			msg.channel.awaitMessages(filter, {
+				max: 1
+			}).then(res => {
+				let file = res.attachments.first().file;
+				fs.readFile(file, (err, data) => {
+					res.channel.send("Read the file! Fetching data...");
+					res.channel.send(data);
+			});
+		});
+	} else {
 		console.log('saw message: ' + msg.content);
 		msg.delete(3000);
 		console.log('deleted message: ' + msg.content);
-	} else if (msg.content.startsWith("sarthak say ") && msg.author.id !== '220352311422091264') {
-		msg.channel.send(msg.content.slice(12, msg.content.length));
-	} else if (msg.content.startsWith("sarthak upload") && msg.author.id !== '220352311422091264') {
-		await msg.channel.send("Send your file please..");
-		let filter = m => m.author.id == msg.author.id; //use that message only if the author is the same
-		msg.channel.awaitMessages(filter, {
-			max: 1
-		}).then(res => {
-			let file = res.attachments.first().file;
-			fs.readFile(file, (err, data) => {
-				res.channel.send("Read the file! Fetching data...");
-				res.channel.send(data);
-			});
-		});
 	}
 });
 
