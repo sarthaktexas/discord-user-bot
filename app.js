92e0732a0e6e87242642aa53eab60f5b6db6c45d
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-04 22:58:44 -0500

    remove autoreplier for now

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ client.on('message', async msg => {
 	if (msg.isMentioned(client.user) && !msg.author.bot && msg.author.id !== '220352311422091264') {
 		// Auto-Replier
 		console.log(msg.author);
-		msg.reply('Hey! I\'m not available at the moment. I\'ll get back to you as soon as possible.');
+		// msg.reply('Hey! I\'m not available at the moment. I\'ll get back to you as soon as possible.');
 	} else if (msg.content.toLowerCase().startsWith("sarthak ping") && msg.author.id !== '220352311422091264') {
 		let start = Date.now();
 	    	msg.channel.sendMessage("*Pinging...*").then((pingRes) => {
