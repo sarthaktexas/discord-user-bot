dab57abfa01010a21d0f8935be4af21875503dc9
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-08 02:14:19 -0500

    whoops update variable name typo

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ client.on('message', async msg => {
 		if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
 		if (amount < 1) return msg.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 		
-		if (message.author.id === '220352311422091264') {
+		if (msg.author.id === '220352311422091264') {
 			msg.delete(); // deletes command message
 		}
 
